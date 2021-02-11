a27422f67c9d299844db952ea33a04ed1fadbfaa
Marcello Santana <marcellosantana@live.com>
2021-02-11 16:32:07 -0300

    adicioanando rotas de filtro por categoria

--- crud/urls.py
+++ crud/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path, include
 from rest_framework import routers
-from .views import CategoryViewSet, ProductViewSet
+from .views import CategoryViewSet, ProductViewSet, ProductByCategoryViewSet
 
 router = routers.DefaultRouter()
-router.register(r'Category', CategoryViewSet)
-router.register(r'Product', ProductViewSet)
+router.register(r'Category', CategoryViewSet, basename='category')
+router.register(r'Product', ProductViewSet, basename='product')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('product/category/<int:id>', ProductByCategoryViewSet.as_view()),
     path('api-auth', include(
         'rest_framework.urls', namespace='rest_framework'
     ))
