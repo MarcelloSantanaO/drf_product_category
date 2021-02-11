271196be34d806538812500857bf11ae13653bfe
Marcello Santana <marcellosantana@live.com>
2021-02-11 16:31:41 -0300

    adicionando as models no painel de administrador

--- crud/admin.py
+++ crud/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from crud.models import Category, Product
 
 
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'id')
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'price')
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product, ProductAdmin)
